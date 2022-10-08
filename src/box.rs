7038295e9f3633963766c475270a8f50dcebf0c8
michigan-musicer <musicer@umich.edu>
2022-10-08 16:04:40 -0400

    move constants up in class declaration, change step size and offset values

--- src/box.rs
+++ src/box.rs
@@ -11,6 +11,18 @@ pub struct Box {
 }
 
 impl Box {
+    const INTERSECT_STEP_SIZE: f32 = 0.01;
+
+    const RESOLVE_OFFSET: f32 = 0.005;
+
+    const NO_INTERSECTION: Vec<cgmath::Vector2<f32> > =  vec![
+        cgmath::Vector2::new(f32::MIN, f32::MIN),
+        cgmath::Vector2::new(f32::MAX, f32::MIN),
+        cgmath::Vector2::new(f32::MIN, f32::MAX),
+        cgmath::Vector2::new(f32::MAX, f32::MAX)
+    ];
+
+
     pub fn new(x: f32, y: f32, width: f32, height: f32) -> Self {
         Self {
             x,
@@ -37,9 +49,6 @@ impl Box {
         return cgmath::Vector2<f32>(width, height);
     }
 
-    // not sure if this is best in terms of code location?
-    const STEP_SIZE: f32 = 0.5;
-
     pub fn points() -> Vec<cgmath::Vector2<f32> > {
         let mut point_set = Vec::new();
         
@@ -48,30 +57,20 @@ impl Box {
             y_current = center.y - height / 2;
             while (y_current != center.y + height / 2) {
                 point_set.push(cgmath::Vector2<f32>(x_current, y_current));
-                y_current = min(y_current + STEP_SIZE, height);
+                y_current = min(y_current + INTERSECT_STEP_SIZE, height);
             }
-            x_current = min(x_current + STEP_SIZE, width);
+            x_current = min(x_current + INTERSECT_STEP_SIZE, width);
         }
 
         return point_set;
-
-        // the below solution probably doesn't return enough points for GLK to work effectively
-        // return vec!
-        // [
-        //     cgmath::Vector2::new(center.x - width / 2, center.y - height / 2),
-        //     cgmath::Vector2::new(center.x - width / 2, center.y + height / 2),
-        //     cgmath::Vector2::new(center.x + width / 2, center.y - height / 2),
-        //     cgmath::Vector2::new(center.x + width / 2, center.y - height / 2)
-        // ]
     }
-
-    // return value is the coordinate of the intersecting point, 
+ 
     pub fn intersect(&a : Box, &b : Box) -> Vec<f32> {
         // this is the GJK algorithm. Minkowski difference is the set of pairwise differences
         // between points in a and points in b.
         // a and b intersect iff origin in Minkowski difference, which we check on line 60
 
-        // return the rectangle that 
+        // return the corners of the intersection rectangle
         x_min = f32::MIN;
         x_max = f32::MAX; 
         y_min = f32::MIN; 
@@ -94,15 +93,6 @@ impl Box {
         ];
     }
 
-    // not sure if this is best in terms of code location?
-    const NO_INTERSECTION: Vec<cgmath::Vector2<f32> > =  vec![
-        cgmath::Vector2::new(f32::MIN, f32::MIN),
-        cgmath::Vector2::new(f32::MAX, f32::MIN),
-        cgmath::Vector2::new(f32::MIN, f32::MAX),
-        cgmath::Vector2::new(f32::MAX, f32::MAX)
-    ];
-
-    const RESOLVE_OFFSET: f32 = 0.05;
 
     pub fn resolve_options(&pusher: Box, &mover: Box) -> Vec<cgmath::Vector2<f32> > {
         options = Vec::new();
